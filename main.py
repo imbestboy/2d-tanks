cd6b33eea60e278ab5fef9bcf5098a553478c2d6
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2023-12-11 11:04:56 +0330

    feat: add main menu

---
+++ main.py
@@ -0,0 +1,7 @@
+import main_menu
+
+# -- create main menu window
+main_menu_window = main_menu.start_main_menu()
+
+# -- keep running
+main_menu_window.mainloop()
---
+++ main_menu.py
@@ -0,0 +1,18 @@
+import customtkinter
+
+import config
+import functions
+
+
+def start_main_menu():
+    # -- create main menu window
+    main_menu_window = customtkinter.CTk()
+
+    # -- main menu window config
+    main_menu_window.geometry(
+        f"{config.MAIN_MENU_SCREEN_WIDTH}x{config.MAIN_MENU_SCREEN_HEIGHT}"
+    )
+    main_menu_window.title("Main Menu")
+    main_menu_window.resizable(False, False)
+
+    return main_menu_window

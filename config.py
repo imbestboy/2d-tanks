b94f24189772390390cbbc536233bb249efe26e0
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2023-12-11 10:35:44 +0330

    feat: add font

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ MAIN_MENU_SCREEN_HEIGHT: int = 400
 # -- theme config
 customtkinter.set_default_color_theme("green")
 customtkinter.set_appearance_mode("system")
+
+# -- creating temp window for config fonts this window will destroy
+temp_window = customtkinter.CTk()
+
+# -- font config
+normal_font = customtkinter.CTkFont("Helvetica", 20)
+
+temp_window.destroy()

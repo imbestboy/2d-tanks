9206fac3b55d89d56ad5c78df1bf2d44e13b6587
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2023-12-11 11:06:56 +0330

    feat: add change theme section to main menu

--- main_menu.py
+++ main_menu.py
@@ -15,4 +15,24 @@ def start_main_menu():
     main_menu_window.title("Main Menu")
     main_menu_window.resizable(False, False)
 
+    # -- change theme section
+    change_theme_frame = customtkinter.CTkFrame(
+        main_menu_window, width=config.MAIN_MENU_SCREEN_WIDTH, fg_color="transparent"
+    )
+    change_theme_frame.pack(pady=config.PADDING_Y)
+
+    customtkinter.CTkLabel(
+        change_theme_frame,
+        text="Choose game theme (default : system) : ",
+        font=config.normal_font,
+    ).grid(column=0, row=0, padx=10)
+    customtkinter.CTkButton(change_theme_frame, text="Dark", command=lambda: 1).grid(
+        column=1, row=0
+    )
+    customtkinter.CTkButton(
+        change_theme_frame,
+        text="Light",
+        command=lambda: 1,
+    ).grid(column=2, row=0, padx=10)
+
     return main_menu_window
